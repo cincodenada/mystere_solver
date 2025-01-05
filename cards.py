7cea86c8d95e55d8d4a72b0d46188dd748f8bc92
Ell Bradshaw <ell@c9a.co>
2025-01-05 03:26:21 -0800

    Generalize number of sides
    WHY

--- cards.py
+++ cards.py
@@ -22,13 +22,14 @@ class Card:
   def __init__(self, idx, sides):
     self.idx = idx
     self.sides = [CardSymbol(side_info) for side_info in sides]
+    self.num_sides = len(self.sides)
 
   @property
   def num(self) -> int:
     return self.idx + 1
 
   def get_side(self, side, rot) -> CardSymbol:
-    return self.sides[(side+rot)%4]
+    return self.sides[(side+rot)%self.num_sides]
 
   def rotated(self, rot):
     return RotatedCard(self, rot)
@@ -37,7 +38,7 @@ class Card:
     return f"{self.num}"
 
   def __str__(self):
-    return f"Card {self.short()}: {','.join([str(self.get_side(idx, 0)) for idx in range(0,4)])}"
+    return f"Card {self.short()}: {','.join([str(self.get_side(idx, 0)) for idx in range(self.num_sides)])}"
 
   def __repr__(self):
     return f"<{str(self)}>"
@@ -46,7 +47,7 @@ class RotatedCard:
   """A Card that has been positioned, and thus has a fixed rotation"""
   def __init__(self, card: Card, rot: int):
     self.card = card
-    self.rot = rot % 4
+    self.rot = rot % self.num_sides
 
   @property
   def ang(self):
@@ -59,20 +60,20 @@ class RotatedCard:
     """Find all Cards in `pool` that can neighbor this card on the given side"""
     match_sym = self.get_side(side)
     # The other card needs to have it on the opposite side
-    match_side = (side + 2) % 4
+    match_side = (side + 2) % self.num_sides
     for card in pool:
       for (target_side, sym) in enumerate(card.sides):
         if sym.matches(match_sym):
           # To get the rotation, subtract target side from unrotated side
-          # So if we find it at the top (side 0) and need it at the left (side 3)
-          # We need to rotate it 3 (3-0)
-          yield RotatedCard(card, (target_side - match_side)%4)
+          # Ex: if we find a match at the top (side 0) and need it positioned
+          # at the left (side 3) then we need to rotate it 3 (3-0)
+          yield RotatedCard(card, (target_side - match_side)%self.num_sides)
 
   def short(self):
     return f"{self.card.num}@{self.ang}"
 
   def __str__(self):
-    return f"Card {self.short()}: {','.join([str(self.get_side(idx)) for idx in range(0,4)])}"
+    return f"Card {self.short()}: {','.join([str(self.get_side(idx)) for idx in range(self.num_sides)])}"
 
   def __repr__(self):
     return f"<{str(self)}>"
@@ -103,7 +104,7 @@ class CardRenderer:
   def borders(self, card):
     if card.card.num not in self.prerendered:
       pad = self.sidelen - 2
-      sides = [(str(card.get_side(n)), '═' if n%2 == 0 else '║') for n in range(4)]
+      sides = [(str(card.get_side(n)), '═' if n%2 == 0 else '║') for n in range(self.num_sides)]
       self.prerendered[card.card.num] = [f"{s:{char}^{pad}}" for (s, char) in sides]
     return self.prerendered[card.card.num]
 
