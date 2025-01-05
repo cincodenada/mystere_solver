4449b96b4a87588d2670bcfc3cc952b058fd655f
Ell Bradshaw <ell@c9a.co>
2025-01-05 00:53:53 -0800

    First run
    This went through a lot

---
+++ cards.py
@@ -0,0 +1,47 @@
+import typing
+
+class CardSymbol:
+  def __init__(self, side_info):
+    (sym, half) = side_info.split('_')
+    self.sym = sym
+    self.half = half
+
+  def matches(self, other):
+    return (other.sym == self.sym and other.half != self.half)
+
+  def __str__(self):
+    return f"{self.sym}_{self.half}"
+
+class Card:
+  def __init__(self, num, sides):
+    self.num = num
+    self.sides = [CardSymbol(side_info) for side_info in sides]
+
+  def get_side(self, side, rot):
+    return self.sides[(side+rot)%4]
+
+  def __str__(self):
+    return f"Card {self.num}: {','.join([str(self.get_side(idx, 0)) for idx in range(0,4)])}"
+
+class RotatedCard:
+  def __init__(self, card, rot):
+    self.card = card
+    self.rot = rot
+
+  def get_side(self, side):
+    return self.card.get_side(side, self.rot)
+
+  def find_valid_neighbors(self, side, pool):
+    match_sym = self.get_side(side)
+    # The other card needs to have it on the opposite side
+    match_side = (side + 2) % 4
+    for card in enumerate(pool):
+      for (target_side, sym) in enumerate(card.sides):
+        if sym.matches(match_sym):
+          # To get the rotation, subtract target side from unrotated side
+          # So if we find it at the top (side 0) and need it at the left (side 3)
+          # We need to rotate it 3 (3-0)
+          yield RotatedCard(card, (match_side - target_side)%4)
+
+  def __str__(self):
+    return f"Card {self.num}@{self.rot}: {','.join([str(self.get_side(idx)) for idx in range(0,4)])}"
---
+++ find.py
@@ -0,0 +1,56 @@
+from cards import Card, RotatedCard
+
+# Cards, with sides clockwise from top
+sidelist = [
+  [ 'dancer_a', 'hair_a', 'bug_a', 'flower_b' ],
+  [ 'bug_b', 'dancer_a', 'hair_b', 'flower_b' ],
+  [ 'dancer_a', 'hair_a', 'flower_a', 'bug_a' ],
+  [ 'flower_b', 'hair_a', 'dancer_a', 'bug_a' ],
+  [ 'hair_b', 'dancer_a', 'bug_b', 'flower_a' ],
+  [ 'dancer_b', 'hair_a', 'bug_a', 'dancer_a' ],
+  [ 'dancer_b', 'bug_a', 'bug_b', 'flower_a' ],
+  [ 'hair_a', 'bug_b', 'flower_a', 'hair_b' ],
+  [ 'flower_a', 'dancer_b', 'flower_a', 'hair_b' ]
+]
+
+# Split them for easier handling
+cards = [Card(num, sides) for num, sides in enumerate(sidelist)]
+
+def find_valid(above_card, left_card, pool):
+  valid_above = set(above_card.find_neighbors(2, pool))
+  valid_left = set(left_card.find_neighbors(1, pool))
+  return valid_above.intersection(valid_left)
+
+def rc_to_idx(row, col):
+  return row*3+col
+
+def valid_next(chosen: list[RotatedCard], remaining: list[Card]):
+  next_pos = len(chosen)
+  row = next_pos // 3
+  col = next_pos % 3
+
+  if row == 0 and col == 0:
+    # First card, all are possible
+    return remaining
+  elif row == 0:
+    # First row, only constraint is left
+    return chosen[rc_to_idx(row, col-1)].find_valid_neighbors(1, remaining)
+  elif col == 0:
+    # First col, only constraint is above
+    return chosen[rc_to_idx(row-1, col)].find_valid_neighbors(2, remaining)
+  else:
+    # Two neighbors
+    return find_valid(
+      chosen[rc_to_idx(row-1, col)],
+      chosen[rc_to_idx(row, col-1)],
+      remaining
+    )
+
+def find_valid(chosen, remaining, target):
+  if(len(chosen) == target):
+    return [chosen]
+
+  return [
+    find_valid(chosen + [candidate], remaining.difference(candidate.card))
+    for candidate in valid_next(chosen, remaining)
+  ]
